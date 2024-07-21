e00542b44faf7060fd42e13611eb40a4388b6496
stangoryan <83424771+stangoryan@users.noreply.github.com>
2024-07-20 18:56:56 -0600

    Add files via upload

--- qqt_diablo-main/scripts/Piteer-main/tasks/enter_portal.lua
+++ qqt_diablo-main/scripts/Piteer-main/tasks/enter_portal.lua
@@ -10,12 +10,16 @@ local task  = {
     Execute = function()
         local portal = utils.get_pit_portal()
         if portal then
-            explorer:clear_path_and_target()
-            explorer:set_custom_target(portal:get_position())
-            explorer:move_to_target()
+            if utils.player_on_quest(enums.quests.pit_ongoing) then
+                explorer:clear_path_and_target()
+                explorer:set_custom_target(portal:get_position())
+                explorer:move_to_target()
 
-            -- Check if the player is close enough to interact with the portal
-            if utils.distance_to(portal) < 2 then
+                -- Check if the player is close enough to interact with the portal
+                if utils.distance_to(portal) < 2 then
+                    interact_object(portal)
+                end
+            else
                 interact_object(portal)
             end
         end
